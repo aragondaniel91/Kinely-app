0b6233b81f50f6ee0f02fbaa6b760e7440599e86
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-04-29 16:13:33 -0500

    Update AuthContext.jsx

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -11,9 +11,7 @@ import { auth, db } from "@/lib/firebase";
 
 const AuthContext = createContext(null);
 
-export function useAuth() {
-  return useContext(AuthContext);
-}
+export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
