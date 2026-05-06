01d1bfa4ebd2a1abcaccdf43c12e1eabde8a6738
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-05 21:08:16 -0500

    Use clickable all-day overflow panel

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import FamilyCalendarView from "@/components/calendar/FamilyCalendarViewV4";
+import FamilyCalendarView from "@/components/calendar/FamilyCalendarViewV5";
 
 const compactCalendarStyles = `
 .family-calendar-shell > div > div.mx-auto {
