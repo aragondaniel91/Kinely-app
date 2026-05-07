6c232cfe3edec3097dcb771bb31fd9d29ab2e91d
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-07 14:03:07 -0500

    Use cleaned family calendar V7

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import FamilyWallClockWeather from "@/components/FamilyWallClockWeather";
-import FamilyCalendarView from "@/components/calendar/FamilyCalendarViewV6";
+import FamilyCalendarView from "@/components/calendar/FamilyCalendarViewV7";
 import CustodyCalendarView from "@/components/calendar/CustodyCalendarView";
 
 const compactCalendarStyles = `
