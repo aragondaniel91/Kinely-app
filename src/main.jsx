c58caf12ba8e172b8d0df94742d333e875e6ef4a
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-08 13:45:40 -0500

    Load forceful week start-time layout patch

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "@/components/calendar/hiddenEventsPopoverPatch.js";
+import "@/components/calendar/weekStartTimeLayoutPatch.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
