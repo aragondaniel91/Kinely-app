39aaaddf12671a1214eff02ea5def916fce4f826
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-08 13:55:42 -0500

    Remove calendar DOM layout patches

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "@/components/calendar/hiddenEventsPopoverPatch.js";
-import "@/components/calendar/weekStartTimeLayoutPatch.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
