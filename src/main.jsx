c595af67110b8b731f04b1ebd61f4e27d27f667e
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-09 00:38:53 -0500

    Load defensive week overlap fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./calendar-cleanup.css";
-import "@/components/calendar/weekHourBucketOverflow.js";
+import "@/components/calendar/weekOverlapFix.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
