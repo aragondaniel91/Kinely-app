14ffae8f07d40738b6171d20ec034013923dd7a4
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-08 20:05:24 -0500

    Load week hour-bucket overflow behavior

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./calendar-cleanup.css";
+import "@/components/calendar/weekHourBucketOverflow.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
