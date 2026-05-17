57d7dda6ac15d6870ae7d03584709a8a70cf795d
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-05-17 15:38:32 -0500

    Load custody card refinement styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./calendar-cleanup.css";
+import "./custody-card-refinement.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
